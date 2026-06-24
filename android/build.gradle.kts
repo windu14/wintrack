3640dbd4837b5ea365acc1d5482d2c3d8b6e5e2c
Windu <windu@wintrack.dev>
2026-06-24 13:38:07 +0700

    Update v4

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            val androidExt = project.extensions.getByName("android")
+            try {
+                androidExt.javaClass.getMethod("setCompileSdk", Int::class.java).invoke(androidExt, 36)
+            } catch (e: Exception) {
+                try {
+                    androidExt.javaClass.getMethod("compileSdkVersion", Int::class.java).invoke(androidExt, 36)
+                } catch (e2: Exception) {
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
